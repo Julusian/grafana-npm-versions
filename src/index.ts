44fc024c47ae3da1fbb14aecb2cf98528fc4c3db
Julian Waller <git@julusian.co.uk>
2020-05-17 00:29:35 +0100

    feat: lint

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
-import { createTerminus, HealthCheckError, TerminusOptions } from '@godaddy/terminus';
+import { createTerminus, HealthCheckError, TerminusOptions } from '@godaddy/terminus'
 import { createServer } from 'http'
 import * as PouchDB from 'pouchdb'
-// import PAll from 'p-all'
 import * as express from 'express'
 import * as reissue from 'reissue'
 import * as Prometheus from 'prom-client'
@@ -11,110 +10,113 @@ import * as PFinally from 'p-finally'
 
 let initialScrapingFinished = false
 async function healthCheck(): Promise<any> {
-    if (!initialScrapingFinished) {
-        throw new HealthCheckError('healthcheck failed', ['Initial scraping not finished']);
-    }
-    return {};
+  if (!initialScrapingFinished) {
+    throw new HealthCheckError('healthcheck failed', ['Initial scraping not finished'])
+  }
+  return {}
 }
 
 async function onSignal(): Promise<any> {
-    console.log('caught signal. Starting cleanup');
+  console.log('caught signal. Starting cleanup')
 }
 
 async function onShutdown(): Promise<any> {
-    console.log('cleanup finished, server is shutting down');
+  console.log('cleanup finished, server is shutting down')
 }
 
 const terminusOptions: TerminusOptions = {
-    healthChecks: {
-        '/healthcheck': healthCheck,
-    },
-    signals: [ 'SIGINT', 'SIGTERM' ],
-    onSignal,
-    onShutdown,
-    logger: console.log,
-};
+  healthChecks: {
+    '/healthcheck': healthCheck,
+  },
+  signals: ['SIGINT', 'SIGTERM'],
+  onSignal,
+  onShutdown,
+  logger: console.log,
+}
 
 const app = express()
-const port = 9600  
-const server = createServer(app);
+const port = 9600
+const server = createServer(app)
 
-createTerminus(server, terminusOptions);
+createTerminus(server, terminusOptions)
 
-const registry = new PouchDB('http://registry.npmjs.com');
+const registry = new PouchDB('http://registry.npmjs.com')
 
 interface PackageInfo {
-    id: string
-    versions: Array<{
-        name: string
-        date: string
-        tag?: string
-    }>
+  id: string
+  versions: Array<{
+    name: string
+    date: string
+    tag?: string
+  }>
 }
 
 let currentData: PackageInfo[] = []
 
-const targetPackages = [
-    'atem-connection',
-    'atem-state',
-    'timeline-state-resolver'
-]
+const targetPackages = ['atem-connection', 'atem-state', 'timeline-state-resolver']
 
-async function doPoll() {
-    console.log('Starting poll')
-    
-    currentData = await Promise.all(targetPackages.map((pkgName) => {
-        return PFinally(PTimeout<PackageInfo>((async () => {
+async function doPoll(): Promise<void> {
+  console.log('Starting poll')
+
+  currentData = await Promise.all(
+    targetPackages.map((pkgName) => {
+      return PFinally(
+        PTimeout<PackageInfo>(
+          (async (): Promise<PackageInfo> => {
             const res: any = await registry.get<any>(pkgName)
 
             const distTags = res['dist-tags']
             return {
-                id: res._id,
-                versions: Object.keys(res.time).map(v => {
-                    const tag = Object.keys(distTags).find(t => distTags[t] === v)
-                    
-                    return {
-                        name: v,
-                        date: res.time[v],
-                        tag: tag
-                    }
-                })
-            }
-        })(), 5000)) as Promise<PackageInfo>
-    }))
+              id: res._id,
+              versions: Object.keys(res.time).map((v) => {
+                const tag = Object.keys(distTags).find((t) => distTags[t] === v)
 
-    initialScrapingFinished = true
-    console.log('Completed poll')
+                return {
+                  name: v,
+                  date: res.time[v],
+                  tag: tag,
+                }
+              }),
+            }
+          })(),
+          5000
+        )
+      ) as Promise<PackageInfo>
+    })
+  )
+
+  initialScrapingFinished = true
+  console.log('Completed poll')
 }
 
 const poller = reissue.create({
-    func: async (callback: () => void) => {
-        try {
-            await doPoll() 
-        } catch (e) {
-            console.error(`Poll threw error: ${e}`)
-        }
-        return callback();
-    },
-    interval: 30000
-});
+  func: async (callback: () => void) => {
+    try {
+      await doPoll()
+    } catch (e) {
+      console.error(`Poll threw error: ${e}`)
+    }
+    return callback()
+  },
+  interval: 30000,
+})
 
-poller.start();
+poller.start()
 
 app.get('/metrics', (_req, res) => {
-    console.log('Got scrape')
+  console.log('Got scrape')
 
-    Metrics.packageVersions.reset()
+  Metrics.packageVersions.reset()
 
-    currentData.forEach(pkg => {
-        pkg.versions.forEach(ver => {
-            if (ver.name === 'created' || ver.name === 'modified') return
-            Metrics.packageVersions.set({ package: pkg.id, version: ver.name, release: ver.date}, 1)
-        })
+  currentData.forEach((pkg) => {
+    pkg.versions.forEach((ver) => {
+      if (ver.name === 'created' || ver.name === 'modified') return
+      Metrics.packageVersions.set({ package: pkg.id, version: ver.name, release: ver.date }, 1)
     })
+  })
 
-    res.set('Content-Type', Prometheus.register.contentType);
-    res.end(Prometheus.register.metrics());
+  res.set('Content-Type', Prometheus.register.contentType)
+  res.end(Prometheus.register.metrics())
 })
 
 server.listen(port, () => console.log(`Listening at http://localhost:${port}`))
