6cfdc73f170b96d9e46f67e791fe5b2cfab14c50
Julian Waller <git@julusian.co.uk>
2020-05-17 15:08:46 +0100

    fix: remove dead code

--- src/metrics.ts
+++
@@ -1,9 +0,0 @@
-import { Gauge } from 'prom-client'
-
-const packageVersions: Gauge<'package' | 'version' | 'release'> = new Gauge({
-  name: 'npm_package_version',
-  help: 'NPM package version',
-  labelNames: ['package', 'version', 'release'],
-})
-
-export { packageVersions }
